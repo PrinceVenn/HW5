e559e095b74bde79bf18b9645fc31bd4605ab8ad
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-10-11 09:06:52 -0700

    Create circles-2.js

---
+++ circles-2.js
@@ -0,0 +1,12 @@
+var sizes = [5, 10, 15, 20, 25, 20, 15, 10, 5];
+
+function setup() {
+  createCanvas(600, 600);
+colorMode(HSB);
+  noStroke();
+    
+  for (var i = 0; i < sizes.length; i += 1) {
+    fill(sizes[i+8], 200, 75);
+    ellipse(50+i*60, height/2, sizes[i], sizes[i]);
+  }
+}
